91b0f48538e6625ea00cdd47911e396ed4e50ce9
nickKearns <nickkearns133@gmail.com>
2021-12-02 15:14:13 -0500

    dynamic data

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,7 +2,7 @@ import './POPOSSpace.css'
 
 
 function POPOSSpace(props) {
-    const { name, image, address } = props
+    const { name, image, address, hours } = props
   return (
     <div className='POPOSSpace'>
       <img src={`${process.env.PUBLIC_URL}/images/${image}`}     
@@ -12,6 +12,7 @@ function POPOSSpace(props) {
       />
       <h1>{name}</h1>
       <div>{address}</div>
+      <div>{hours}</div>
     </div>
   )
 }
