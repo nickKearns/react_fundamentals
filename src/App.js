a3e7d492e79ea290a8baa07ff570ea94c4a559dc
nickKearns <nickkearns133@gmail.com>
2021-12-02 14:29:38 -0500

    content component built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title.js';
+import POPOSList from './POPOSList';
 
 function App() {
 return (
     <div className="App">
       <Title />
+      <POPOSList />
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
